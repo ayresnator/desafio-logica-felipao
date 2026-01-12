058b6bff7be15931c09ae5bbe40cd16583813e7a
ayresnator <anaayreso@hotmail.com>
2026-01-12 15:18:42 -0300

    Código do desafio criado e rodado com sucesso

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+let nomeHeroi = "Sr. Arroganto";
+let quantidadeXP = 10000;
+
+switch (quantidadeXP) {
+  case quantidadeXP <1000: "Ferro"
+  break;
+  case quantidadeXP >=1000 && quantidadeXP <2000: "Bronze"
+  break;
+  case quantidadeXP >=2000 && quantidadeXP <5000: "Prata"
+  break;
+  case quantidadeXP >=5000 && quantidadeXP <7000: "Ouro"
+  break;
+  case quantidadeXP >=7000 && quantidadeXP <8000: "Platina"
+  break;
+  case quantidadeXP >=8000 && quantidadeXP <9000: "Ascendente"
+  break;
+  case quantidadeXP >=9000 && quantidadeXP <10000: "Imortal"
+  break;
+  case quantidadeXP >=10000: "Radiante"
+  default: "Desconhecido"
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nível de ${quantidadeXP}`)
